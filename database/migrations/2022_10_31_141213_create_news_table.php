0bed263b5c3445866154e9c67cee9a2f274da697
Miroslav Milosevic <miroslav.milosevic@mrgeorge.onmicrosoft.com>
2022-10-31 16:32:46 +0100

    second commit

---
+++ database/migrations/2022_10_31_141213_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 64)->nullable();
+            $table->string('description', 255)->nullable();
+            $table->string('text', 2500)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+};
